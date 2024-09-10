a08ff01e9dda581e22398d09d8e9f5c20db89f5f
megavolkan <megavolkan@gmail.com>
2024-09-10 15:38:14 +0300

    Gulp iconfont kaldırıldı

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,6 @@ const concat = require('gulp-concat')
 const terser = require('gulp-terser')
 const fileinclude = require('gulp-file-include')
 const rename = require('gulp-rename')
-const iconfont = require('gulp-iconfont');
-const iconfontCss = require('gulp-iconfont-css');
 const browserSync = require('browser-sync').create()
 
 
@@ -96,31 +94,6 @@ function watchTask() {
   watch('src/scripts/**/*.js', series(jsTask, browsersyncReload))
 }
 
-// Icon font derleyici
-const fontName = 'iconfont'
-const runTimestamp = Math.round(Date.now() / 1000);
-
-function iconfontTask(done) {
-  return src(['src/icons/*.svg'])
-    .pipe(iconfontCss({
-      fontName: fontName,
-      //path: 'src/icons/templates/_icons.scss',
-      path: 'src/icons/templates/_icons.css',
-      targetPath: '../../../../src/scss/inc/_iconfont.scss',
-      fontPath: '../fonts/iconfont/'
-    }))
-    .pipe(iconfont({
-      fontName: fontName,
-      prependUnicode: true,
-      formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
-      timestamp: runTimestamp, // recommended to get consistent builds when watching files
-      normalize: true,
-      fontHeight: 1001
-    }))
-    .pipe(dest('dist/assets/fonts/iconfont/'))
-  done()
-}
-
 // Customize Bootstrap SCSS
 function customizeBootstrap() {
   return src('src/scss/vendor/bootstrap_source/bootstrap.scss')
@@ -139,8 +112,6 @@ exports.default = series(
   watchTask,
 )
 
-// ICONFONT: Çalıştırmak için terminalde: gulp iconfont
-exports.iconfont = series(iconfontTask, bsScssTask)
 
 // CUSTOMIZE BOOTSTRAP: Çalıştırmak için terminalde: gulp bootstrap
 exports.bootstrap = series(customizeBootstrap, bsScssTask)
